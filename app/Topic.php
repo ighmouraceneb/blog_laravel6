290902e841e8456c2714b7a4f9be08c39245d666
ighmouraceneb <ighmouraceneb@yahoo.fr>
2019-10-28 17:14:47 +0100

    add eager loader class for optimisation request eloquent

--- app/Topic.php
+++ app/Topic.php
@@ -2,11 +2,22 @@
 
 namespace App;
 
+use App\Post;
 use Illuminate\Database\Eloquent\Model;
 
 class Topic extends Model
 {
-    protected $fillable = [
-        'title', 'slug', 'user_id'
-    ];
+    protected $fillable = ['title', 'slug', 'user_id'];
+
+
+    /**
+     * relation : a topic has manay posts
+     *
+     * @return void
+     */
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
 }
--- app/User.php
+++ app/User.php
@@ -5,6 +5,8 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Post;
+
 
 class User extends Authenticatable
 {
@@ -36,4 +38,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    
+    /**
+     * relation : a user has manay posts
+     *
+     * @return object
+     */
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
 }
